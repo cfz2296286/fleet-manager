73932d839649907b016ccb5877190d7f3584e1e6
cfz2296286 <cfz2296286@gmail.com>
2023-07-11 21:17:00 +1000

    Update vite.config.js
    updated base

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: './',
+  base: '',
   server: {
     host: "0.0.0.0",
     port: 8080,
