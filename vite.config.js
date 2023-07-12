ec3e3343d96b390dbb11ca3d616b505264f9e90d
cfz2296286 <cfz2296286@gmail.com>
2023-07-12 17:49:44 +1000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   base: './',
   server: {
     host: "0.0.0.0",    
-    port: 3000,
+    port: 8888,
     proxy: {
       '^/server/.*': {
         target: 'http://54.151.125.63:8000/',
