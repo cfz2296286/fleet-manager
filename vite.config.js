4852e122d1d93b76dd79d2ca22bfced61a7cc64b
cfz2296286 <cfz2296286@gmail.com>
2023-07-12 17:40:52 +1000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,8 +7,6 @@ export default defineConfig({
   plugins: [vue()],
   base: './',
   server: {
-    host: "0.0.0.0",    
-    port: 8080,
     proxy: {
       '^/server/.*': {
         target: 'http://54.151.125.63:8000/',
